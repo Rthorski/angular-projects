e933d66d28ee91de305418c0986634707fd9cf8f
Rthorski <alexandre.rethore@icloud.com>
2024-07-17 13:32:14 +0200

    quete 3.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Alex';
+  title: string = 'Raclette Party 🧀';
+
+  isAdmin: boolean = true;
+
+  changeIsAdmin(): void {
+    this.isAdmin = !this.isAdmin;
+  }
 }
